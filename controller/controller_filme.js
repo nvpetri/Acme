807082d0a5237e35b6cf5dfc11de0443b5f8e45f
NPS <nicolaspetri18@gmail.com>
2024-01-30 17:07:34 -0300

    iniciando DAO

---
+++ controller/controller_filme.js
@@ -0,0 +1,29 @@
+/*****************************************************************************************************
+ * Objetivo: Arquivo responsável por realizar validações, consistencias e regras de negócio
+ * para os filmes
+ * Data: 30/01/2024
+ * Autor: Nicolas Vasconcelos
+ * Versão: 1.0
+ ****************************************************************************************************/
+
+// Função para add novo filme
+const setNovoFilme = async function() {
+
+}
+
+
+const setAtualizarFilme = async function() {
+
+}
+
+const setExcluirFilme = async function() {
+
+}
+
+const getListarFilmes = async function() {
+
+}
+
+const getBuscarFilme = async function() {
+
+}
