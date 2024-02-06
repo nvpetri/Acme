36e5baeee3aaf26e0408801ea218083635555547
NPS <nicolaspetri18@gmail.com>
2024-02-06 14:44:08 -0300

    dppp

--- model/DAO/filme.js
+++ model/DAO/filme.js
@@ -5,6 +5,10 @@
  * Versão: 1.0
  ****************************************************************************************************/
 
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
 const insertFilme = async function() {
 
 }
@@ -18,7 +22,17 @@ const deleteFilme = async function() {
 }
 
 const selectAllFilmes = async function() {
+    let sql = 'select * from tbl_filme'
 
+    /**
+     * $queryRawUnsafe(sql) --encaminha uma variavel
+     * $queryRaw('select * from tbl_filme') -- encaminha o script direto
+     */
+
+    let rsFilmes = await prisma.$queryRawUnsafe(sql)
+
+    if (rsFilmes.length > 0) return rsFilmes
+    else return false
 }
 
 const selectFilmeById = async function() {
