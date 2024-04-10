ec029887757dc17f39e2ea4341f3e9e2feee33c7
nvpetri <nicolaspetri18@gmail.com>
2024-04-10 13:00:17 -0300

    implementação: estruturando banco de dados para os novos requisitos

--- database/db_acme_filmes.sql
+++ database/db_acme_filmes.sql
@@ -2,6 +2,17 @@ create schema db_acme_filmes_turma_aa;
 
 use db_acme_filmes_turma_aa;
 
+create table tbl_classificacao(
+id int not null auto_increment primary key,
+nome varchar(45) not null,
+sigla varchar(5) not null,
+descricao varchar(100) not null,
+icon text,
+
+unique key (id),
+unique index (id)
+);
+
 create table tbl_filme(
 	id int not null auto_increment primary key,
     nome varchar(80) not null,
@@ -11,31 +22,82 @@ create table tbl_filme(
     data_relancamento date,
     foto_capa varchar(300) not null,
     valor_unitario float,
+    id_classificacao int not null,
     
     unique key (id),
-    unique index (id)
+    unique index (id),
+    
+    foreign key(id_classificacao) references tbl_classificacao(id)
 );
 
-insert into tbl_filme (nome, sinopse, duracao, data_lancamento, data_relancamento, foto_capa, valor_unitario) values
-(
-"O Segredo do Vale",
-"Um drama emocionante que explora os segredos de uma pequena cidade no interior.",
-'02:35:00',
-"2022-08-25",
-null,
-"https://exemplo.com/foto_vale.jpg",
-29.99
-),
-(
-"Carros",
-"Um carro com vida, correndo.",
-'02:10:00',
-"2021-12-15",
-null,
-"https://exemplo.com/foto_vale.jpg",
-49.99
+create table tbl_genero (
+	id int not null auto_increment primary key,
+    nome varchar(45),
+    
+    unique key (id),
+	unique index (id)    
 );
+
+create table tbl_filme_genero (
+	id int not null auto_increment primary key,
+    id_genero int not null,
+    id_filme int not null,
+    
+    unique key (id),
+    unique index(id),
+    
+    foreign key(id_genero) references tbl_genero(id),
+    foreign key(id_filme) references tbl_filme(id)
+);
+
+create table tbl_sexo(
+	id int not null auto_increment primary key,
+    nome varchar(2) not null,
+    
+    unique key (id),
+    unique index(id)
+);
+
+create table tbl_nacionalidade(
+	id int not null auto_increment primary key,
+    nome varchar(45) not null,
+    
+    unique key (id),
+    unique index(id)
+);
+
+create table tbl_diretores(
+	id int not null auto_increment primary key,
+    nome varchar(90) not null,
+    nome_artistico varchar(90),
+    biografia text,
+    foto text,
+    data_nascimento date not null,
+    data_falecimeto date,
+    id_sexo int not null,
+    
+    unique key (id),
+    unique index(id),
+    
+    foreign key(id_sexo) references tbl_sexo(id)
+);
+
+create table tbl_atores(
+	id int not null auto_increment primary key,
+    nome varchar(90) not null,
+    nome_artistico varchar(90),
+    biografia text,
+    foto text,
+    data_nascimento date not null,
+    data_falecimeto date,
+    id_sexo int not null,
+    
+    unique key (id),
+    unique index(id),
+    
+    foreign key(id_sexo) references tbl_sexo(id)
+);
+
+
 
 show tables;
-
-select * from tbl_filme where nome like "%Segredo%"
